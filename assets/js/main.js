925fd1d046e5f280ad3aeb30803aab77a21b1534
Mridul Seth <seth.mridul+github@gmail.com>
2020-01-08 15:40:03 +0530

    remove whitespace

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,7 +24,7 @@ function launchNotebook(url, platform) {
     ghFilePath;
 
   var customLauncherDomain = document.getElementById('customLauncherDomain')
-    .textContent;
+    .textContent.trim();
 
   var econarkURL =
     customLauncherDomain +
